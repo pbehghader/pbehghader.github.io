ef51e736c098fcb7b9be0cbe0afaa6c8891df176
Pooyan Behnamghader <behnamghader@gmail.com>
2015-05-22 13:39:01 +0200

    Updated

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
         <meta name="keywords" content="your,keywords,goes,here" />
         <meta name="author" content="Pooyan Behnamghader" />
         <link rel="stylesheet" type="text/css" href="variant-light.css" title="Variant Light" media="screen,projection" />
-        <link rel="shortcut icon" href="http://sstatic.net/stackoverflow/img/favicon.ico">
+        <link rel="icon" href="http://www.cs.usc.edu/favicon.ico" type="image/x-icon">
         <title>Pooyan Behnamghader</title>
     </head>
 
