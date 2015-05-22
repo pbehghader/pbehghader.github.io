cb1c555a872766ede5ef558e00a5ca89cf814097
Pooyan Behnamghader <behnamghader@gmail.com>
2015-05-22 18:41:28 +0200

    Updated

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,8 @@
 <div id="sidebar">
     <ul class="sidemenu">
         <li><img src="images/usc.jpg" width="220" alt="University of Southern California" /></li>
+        <li><img src="images/zurich.jpg" width="220" alt="College of Engineering, University of Tehran"/></li>
         <li><img src="images/unitehran.jpg" width="220" alt="Universit of Tehran" /></li>
-        <!--
-        <li><img src="images/fanni.jpg" width="220" alt="College of Engineering, University of Tehran"/></li>
-        -->
         <li><img src="images/epfl.jpg" width="220" alt="École Polytechnique Fédérale de Lausanne (EPFL),""/></li>
         <li><img src="images/sampad.jpg" width="220" alt="National Organization for Development of Exceptional Talents"/></li>
         
