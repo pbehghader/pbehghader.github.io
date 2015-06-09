fabf50d1f70e44345c1bd479df58586f6bde0fac
Pooyan Behnamghader <behnamghader@gmail.com>
2015-06-09 17:30:17 +0200

    change

--- publications.php
+++ publications.php
@@ -18,10 +18,10 @@
             <?php include 'sidebar.php'; ?>
             <div id="content">
                 <div class="post">
-                    <h2>Related Courses</h2>
+                    <h2>Publications</h2>
                     <div>
                         <h3>
-                            Software Engineering and Formal Methods
+                            2015
                         </h3>
                         <ul class="noBullet">
                             <li>
@@ -41,122 +41,7 @@
 
                         </ul>
                     </div>
-                    <div>
-                        <h3>
-                            Programming Languages, Compiler Design, and Object-Oriented Systems
-                        </h3>
-                        <ul class="noBullet">
-                            <li>
-                                <p>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Programming Languages Analysis & Design
-                                    <br/>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Compiler Design and Construction
-                                    <br/>
-                                    <div class="grade"><strong>19.5</strong> (of 20) </div><div class="description"><strong>Second Mark</strong></div> Advance Programming
-                                    <br/>
-                                    <div class="grade"><strong>17.15</strong> (of 20) </div><div class="description"><strong></strong></div> Operating Systems (and Laboratory)
-                                    <br/>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Computer Architecture
-                                    <br/>
-                                    <div class="grade"><strong>17.5</strong> (of 20) </div><div class="description"><strong>Third Mark</strong></div> Basics of Computer Programming
-                                    <br/>
-
-                                </p>
-                            </li>
-
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>
-                            Computer Networks, Distributed Systems, and Security
-                        </h3>
-                        <ul class="noBullet">
-                            <li>
-                                <p>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Computer Networks
-                                    <br/>
-                                    <div class="grade"><strong>19.5</strong> (of 20) </div><div class="description"><strong>Second Mark</strong></div> Internet Engineering
-                                    <br/>
-                                </p>
-                            </li>
-
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>
-                            Algorithms and Mathematics
-                        </h3>
-                        <ul class="noBullet">
-                            <li>
-                                <p>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Algorithm Design
-                                    <br/>
-                                    <div class="grade"><strong>19.1</strong> (of 20) </div><div class="description"><strong>Third Mark</strong></div> Data Structure
-                                    <br/>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Discrete Mathematical Structure
-                                    <br/>
-                                    <div class="grade"><strong>19</strong> (of 20) </div><div class="description"><strong>Third Mark</strong></div> Engineering Mathematics
-                                    <br/>
-                                    <div class="grade"><strong>18</strong> (of 20) </div><div class="description"><strong></strong></div> Numerical Analysis Methods
-                                    <br/>
-                                    <div class="grade"><strong>18.4</strong> (of 20) </div><div class="description"><strong></strong></div> Engineering Probability & Statistics
-                                    <br/>
-                                    <div class="grade"><strong>18</strong> (of 20) </div><div class="description"><strong></strong></div> Differential Equations
-                                    <br/> 
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>
-                            Artificial Intelligence
-                        </h3>
-                        <ul class="noBullet">
-                            <li>
-                                <p>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Artificial Intelligence
-                                    <br/>
-                                    <div class="grade"><strong>18.2</strong> (of 20) </div><div class="description"><strong></strong></div> Intelligence Systems
-                                    <br/>
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>
-                            Hardware and Electrical Engineering
-                        </h3>
-                        <ul class="noBullet">
-                            <li>
-                                <p>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Computer Logics (and Laboratory)
-                                    <br/>
-                                    <div class="grade"><strong>19.5</strong> (of 20) </div><div class="description"><strong>Third Mark</strong></div> Microprocessors
-                                    <br/>
-                                    <div class="grade"><strong>19.7</strong> (of 20) </div><div class="description"><strong>Second Mark</strong></div> Electronics
-                                    <br/>
-                                    <div class="grade"><strong>17.75</strong> (of 20) </div><div class="description"></div> Electrical Circuits (and Laboratory)
-                                    <br/>
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>
-                            Other Related Courses to Computer Science and Engineering
-                        </h3>
-                        <ul class="noBullet">
-                            <li>
-                                <p>
-                                    <div class="grade"><strong>18.75</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Computer Graphics
-                                    <br/>
-                                    <div class="grade"><strong>20</strong> (of 20) </div><div class="description"><strong>First Mark</strong></div> Sci. and Tech. Communication
-                                    <br/>
-                                </p>
-                            </li>
-                        </ul>
-                    </div>
-
+                    
                 </div>
             </div>
         </div>
