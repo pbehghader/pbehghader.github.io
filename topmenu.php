88373f541b689dddd180ee1c2afab36153247c27
Pooyan Behnamghader <behnamghader@gmail.com>
2015-06-09 17:41:39 +0200

    change

--- topmenu.php
+++ topmenu.php
@@ -4,6 +4,7 @@
         <a href="home.php">Home</a><span class="hide"> | </span>
         <a href="about.php">About</a><span class="hide"> | </span>
         <a href="resume.php">Resume</a><span class="hide"> | </span>
+        <a href="publications.php">Publicatoins</a><span class="hide"> | </span>
         <!--
         <a href="courses.php">Related Courses</a><span class="hide"> | </span>
         <a href="photos.php">Photos</a><span class="hide"> | </span>
