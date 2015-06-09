7e5843c485aa42fa8e05ce4056185b69f195aa4e
Pooyan Behnamghader <behnamghader@gmail.com>
2015-06-09 17:22:34 +0200

    MSR 15 added

--- resume.php
+++ resume.php
@@ -280,14 +280,15 @@
                             <ul>
                                 <li>
                                     <p>
-                                    Duc Le*, <strong>Pooyan Behnamghader* (co-first author)</strong>, Joshua Garcia, Daniel Link, Arman Shahbazian and Nenad Medvidovic. “An Empirical Study of Architectural Change in Open-Source Software Systems.” In <i>Proceedings of the 12th Working Conference on Mining Software Repositories (MSR 2015),</i> Florence, Italy, May 2015.
+                                    Duc Le*, <strong>Pooyan Behnamghader* (co-first author)</strong>, Joshua Garcia, Daniel Link, Arman Shahbazian and Nenad Medvidovic. “<a href="publications/An Empirical Study of Architectural Change in Open-Source Software Systems.pdf">An Empirical Study of Architectural Change in Open-Source Software Systems</a>.” In <i>Proceedings of the 12th Working Conference on Mining Software Repositories (MSR 2015),</i> Florence, Italy, May 2015.
                                     <!--
                                         M. Moghaddami, <strong>P. Behnamghader</strong>, M. Raisee, "Effects of vortex shedding on the UV-H2O2 photoreactor performance"	<strong>to be submitted</strong>
                                     -->
                                     </p>
                                 </li>
                             </ul>
-                        
+                            
+                         
                     </div>
                     
                     <div>
