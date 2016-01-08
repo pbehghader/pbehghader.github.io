ff23c04f11861f1b11d98df2edb41cd076746323
pooyan <behnamghader@gmail.com>
2016-01-07 22:02:20 -0800

    small fix in the content

--- home.php
+++ home.php
@@ -26,10 +26,12 @@
                     <p>My name is Pooyan Behnamghader. I am a PhD student in Computer Science at University of Southern California. My advisor is <a target="_blank" href="http://sunset.usc.edu/~neno/">Nenad Medvidovic</a>. I am a member of the <a target="_blank" href="http://softarch.usc.edu/">Software Architecture Research Group</a> at the <a target="_blank" href="http://sunset.usc.edu/new/index.php">Center for Systems and Software Engineering</a> at USC.</p>
                     -->
 
-                    <p class="justified"> I am a third year PhD student in the <a target="_blank" href="http://cs.usc.edu">Computer Science Department</a> at the <a target="_blank" href="http://usc.edu">University of Southern California</a>.
-                        I work in <a target="_blank" href="http://softarch.usc.edu/">Software Architecture Research Group</a> under the supervision of Prof. <a target="_blank" href="http://sunset.usc.edu/~neno/">Nenad Medvidovic</a> at the <a target="_blank" href="http://sunset.usc.edu/new/index.php">Center for Systems and Software Engineering</a>.                         </p>
+                    <p class="justified"> 
+                        Mr. Behnamghader is a third year PhD student in the <a target="_blank" href="http://cs.usc.edu">Computer Science Department</a> at the <a target="_blank" href="http://usc.edu">University of Southern California</a>.
+                        He works in <a target="_blank" href="http://softarch.usc.edu/">Software Architecture Research Group</a> under the supervision of Prof. <a target="_blank" href="http://sunset.usc.edu/~neno/">Nenad Medvidovic</a> at the <a target="_blank" href="http://sunset.usc.edu/new/index.php">Center for Systems and Software Engineering</a>.
+                    </p>
 
-                    <p class="justified">My major areas of interest are:
+                    <p class="justified">His major areas of interest are:
                         <ul>
                             <li>Software Architecture Recovery, Software Repository Mining</li>
                             <li>Distributed Systems, Cloud Computing</li>
@@ -37,9 +39,9 @@
                     </p>
                     <p class="justified">
                         
-                        I received my B.S. in Computer Engineering from the <a target="_blank" href="http://ece.ut.ac.ir/en">School of Electrical and Computer Engineering</a> at <a target="_blank" href="http://ut.ac.ir/en">University of Tehran</a> under the supervision of Dr. <a target="_blank" href="http://ece.ut.ac.ir/en/users/fghassemi">Fatemeh Ghassemi</a> in June 2013.
-                        I was a visiting PhD student in <a target="_blank" href="http://www.ifi.uzh.ch/seal.html">Software Architecture and Evolution Lab</a>, led by Prof. <a target="_blank" href="http://www.ifi.uzh.ch/seal/people/gall.html">Harald Gall</a>, at the <a target="_blank" href="http://www.uzh.ch/index_en.html">University of Zurich</a> for the summer of 2015.
-                        In addition, I worked as a research assistant intern in the <a target="_blank" href="http://ic.epfl.ch/">School of Computer and Communication</a> at <a target="_blank" href="http://epfl.ch/">EPFL</a> under the supervision of Prof. <a target="_blank" href="http://ltiwww.epfl.ch/~petitp/">Claude Petitpierre</a> in summer 2012.
+                        Mr. Behnamghader received my B.S. in Computer Engineering from the <a target="_blank" href="http://ece.ut.ac.ir/en">School of Electrical and Computer Engineering</a> at <a target="_blank" href="http://ut.ac.ir/en">University of Tehran</a> under the supervision of Dr. <a target="_blank" href="http://ece.ut.ac.ir/en/users/fghassemi">Fatemeh Ghassemi</a> in June 2013.
+                        He worked as a visiting PhD student in <a target="_blank" href="http://www.ifi.uzh.ch/seal.html">Software Architecture and Evolution Lab</a>, led by Prof. <a target="_blank" href="http://www.ifi.uzh.ch/seal/people/gall.html">Harald Gall</a>, at the <a target="_blank" href="http://www.uzh.ch/index_en.html">University of Zurich</a> for the summer of 2015.
+                        In addition, He worked as a research assistant intern in the <a target="_blank" href="http://ic.epfl.ch/">School of Computer and Communication</a> at <a target="_blank" href="http://epfl.ch/">EPFL</a> under the supervision of Prof. <a target="_blank" href="http://ltiwww.epfl.ch/~petitp/">Claude Petitpierre</a> in summer 2012.
                     </p>
                     <p class="justified">
                         Find my Resume at <a target="_blank" href="resume.php"><strong>web</strong></a> / <a target="_blank" href="PooyanBehnamghader.pdf"><strong>pdf</strong></a>.
