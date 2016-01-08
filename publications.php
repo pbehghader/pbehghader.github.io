b8000b8c4844adaba3a30cee19946f0429e06828
pooyan <behnamghader@gmail.com>
2016-01-07 22:17:44 -0800

    small fix in the content

--- publications.php
+++ publications.php
@@ -54,7 +54,7 @@ of a system’s implementation-level structure as a proxy for its architecture.
                                  <li>
                                     <p>
 
-                                    Duc Le*, <strong>Pooyan Behnamghader* (co-first author)</strong>, Joshua Garcia, Daniel Link, Arman Shahbazian and Nenad Medvidovic. “<a href="publications/emparch_msr15.pdf">An Empirical Study of Architectural Change in Open-Source Software Systems</a>.” In <i>Proceedings of the 12th Working Conference on Mining Software Repositories (MSR 2015),</i> Florence, Italy, May 2015.
+                                    Sonal Mahajan, Bailan Li, <strong>Pooyan Behnamghader</strong>, William G. J. Halfond. “Using Visual Symptoms for Debugging Presentation Failures in Web Applications.” In <i>Proceedings of the 9th IEEE International Conference on Software Testing, Verification and Validation (ICST 2016),</i> Chicago, USA, April 2016. (To appear)
                                     <!--
                                         M. Moghaddami, <strong>P. Behnamghader</strong>, M. Raisee, "Effects of vortex shedding on the UV-H2O2 photoreactor performance"    <strong>to be submitted</strong>
                                     -->
@@ -63,12 +63,8 @@ of a system’s implementation-level structure as a proxy for its architecture.
                                     <div class="workDescription">
                                     <ul >
                                         <li>
-                                        <b>Abstract</b>-From its very inception, the study of software architecture has recognized architectural decay as a regularly occurring phenomenon in long-lived systems. Architectural decay
-is caused by repeated changes to a system during its lifespan. Despite decay’s prevalence, there is a relative dearth of empirical data regarding the nature of architectural changes that may lead
-to decay, and of developers’ understanding of those changes. In this paper, we take a step toward addressing that scarcity by conducting an empirical study of changes found in software
-architectures spanning several hundred versions of 14 opensource systems. Our study reveals several new findings regarding the frequency of architectural changes in software systems, the
-common points of departure in a system’s architecture during maintenance and evolution, the difference between system-level and component-level architectural change, and the suitability
-of a system’s implementation-level structure as a proxy for its architecture.
+                                            <b>Abstract</b>—Presentation failures in a website can undermine its success by giving users a negative perception of the trustwor- thiness of the site and the quality of the services it delivers. Unfortunately, existing techniques for debugging presentation failures do not provide developers with automated and broadly applicable solutions for finding the site’s faulty HTML elements and CSS properties. To address this limitation, we propose a novel automated approach for debugging web sites that is based on image processing and probabilistic techniques. Our approach first builds a model that links observable changes in the web site’s appearance to faulty elements and styling properties. Then using this model, our approach predicts the elements and styling properties most likely to cause the observed failure for the page under test and reports these to the developer. In evaluation, our approach was more accurate and faster than prior techniques for identifying faulty elements in a website.
+                                        
                                         </li>
                                         
                                     </ul>
