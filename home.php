93b254f7b89a3fb691eb28e2a71b7e38515a39e9
pooyan <behnamghader@gmail.com>
2016-01-07 21:55:59 -0800

    small fix in the content

--- home.php
+++ home.php
@@ -26,7 +26,7 @@
                     <p>My name is Pooyan Behnamghader. I am a PhD student in Computer Science at University of Southern California. My advisor is <a target="_blank" href="http://sunset.usc.edu/~neno/">Nenad Medvidovic</a>. I am a member of the <a target="_blank" href="http://softarch.usc.edu/">Software Architecture Research Group</a> at the <a target="_blank" href="http://sunset.usc.edu/new/index.php">Center for Systems and Software Engineering</a> at USC.</p>
                     -->
 
-                    <p class="justified"> I am a second year PhD student in the <a target="_blank" href="http://cs.usc.edu">Computer Science Department</a> at the <a target="_blank" href="http://usc.edu">University of Southern California</a>.
+                    <p class="justified"> I am a third year PhD student in the <a target="_blank" href="http://cs.usc.edu">Computer Science Department</a> at the <a target="_blank" href="http://usc.edu">University of Southern California</a>.
                         I work in <a target="_blank" href="http://softarch.usc.edu/">Software Architecture Research Group</a> under the supervision of Prof. <a target="_blank" href="http://sunset.usc.edu/~neno/">Nenad Medvidovic</a> at the <a target="_blank" href="http://sunset.usc.edu/new/index.php">Center for Systems and Software Engineering</a>. 
                         Currently, I am a visiting researcher in <a target="_blank" href="http://www.ifi.uzh.ch/seal.html">Software Architecture and Evolution Lab</a>, led by Prof. <a target="_blank" href="http://www.ifi.uzh.ch/seal/people/gall.html">Harald Gall</a>, at the <a target="_blank" href="http://www.uzh.ch/index_en.html">University of Zurich</a> for the summer of 2015.</p>
 
