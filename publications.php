43b35b928eb8930aab402e1cace59fdf8f50f320
pooyan <behnamghader@gmail.com>
2016-01-07 22:15:08 -0800

    small fix in the content

--- publications.php
+++ publications.php
@@ -50,6 +50,32 @@ of a system’s implementation-level structure as a proxy for its architecture.
 
                                     </p>
                                 </li>
+
+                                 <li>
+                                    <p>
+
+                                    Duc Le*, <strong>Pooyan Behnamghader* (co-first author)</strong>, Joshua Garcia, Daniel Link, Arman Shahbazian and Nenad Medvidovic. “<a href="publications/emparch_msr15.pdf">An Empirical Study of Architectural Change in Open-Source Software Systems</a>.” In <i>Proceedings of the 12th Working Conference on Mining Software Repositories (MSR 2015),</i> Florence, Italy, May 2015.
+                                    <!--
+                                        M. Moghaddami, <strong>P. Behnamghader</strong>, M. Raisee, "Effects of vortex shedding on the UV-H2O2 photoreactor performance"    <strong>to be submitted</strong>
+                                    -->
+                                     <!-- Buffer -->
+                                     (<a href="publications/emparch_msr15.pdf"><b>Download</b></a>)
+                                    <div class="workDescription">
+                                    <ul >
+                                        <li>
+                                        <b>Abstract</b>-From its very inception, the study of software architecture has recognized architectural decay as a regularly occurring phenomenon in long-lived systems. Architectural decay
+is caused by repeated changes to a system during its lifespan. Despite decay’s prevalence, there is a relative dearth of empirical data regarding the nature of architectural changes that may lead
+to decay, and of developers’ understanding of those changes. In this paper, we take a step toward addressing that scarcity by conducting an empirical study of changes found in software
+architectures spanning several hundred versions of 14 opensource systems. Our study reveals several new findings regarding the frequency of architectural changes in software systems, the
+common points of departure in a system’s architecture during maintenance and evolution, the difference between system-level and component-level architectural change, and the suitability
+of a system’s implementation-level structure as a proxy for its architecture.
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+
+                                    </p>
+                                </li>
                             </ul>
                     
                     </div>
