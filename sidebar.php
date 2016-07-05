bedfad1b07a3cce87a11dcaafa85f54789788ea6
pooyan <behnamghader@gmail.com>
2016-07-04 17:26:42 -0700

    update the side bar

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,9 @@
 <div id="sidebar">
     <ul class="sidemenu">
         <li><img src="images/usc.jpg" width="220" alt="University of Southern California" /></li>
+        <li><img src="images/zurich.jpg" width="220" alt="College of Engineering, University of Tehran"/></li>
         <li><img src="images/ambermelon.jpg" width="220" alt="AmberMelon Smart Watch" /></li>
         <li><img src="images/huntus.jpg" width="220" alt="Huntus Platform" /></li>
-        <li><img src="images/zurich.jpg" width="220" alt="College of Engineering, University of Tehran"/></li>
         <li><img src="images/unitehran.jpg" width="220" alt="University of Tehran" /></li>
         <li><img src="images/epfl.jpg" width="220" alt="École Polytechnique Fédérale de Lausanne (EPFL),"/></li>
         <li><img src="images/sampad.jpg" width="220" alt="National Organization for Development of Exceptional Talents"/></li>
