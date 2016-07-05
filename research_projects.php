284b6543af52348107b6be7bde0181bdb9295a48
pooyan <behnamghader@gmail.com>
2016-07-04 18:17:19 -0700

    update the top menu

---
+++ research_projects.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php include 'analytics.php'; ?>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="Your description goes here" />
+        <meta name="keywords" content="your,keywords,goes,here" />
+        <meta name="author" content="Pooyan Behnamghader" />
+        <link rel="stylesheet" type="text/css" href="variant-light.css" title="Variant Light" media="screen,projection" />
+        <link rel="icon" href="http://www.cs.usc.edu/favicon.ico" type="image/x-icon">
+        <title>Publications</title>
+    </head>
+
+    <body>
+        <?php include 'header.php'; ?>
+        <?php include 'topmenu.php'; ?>
+        <div id="wrap">
+            <?php include 'sidebar.php'; ?>
+            <div id="content">
+                <div class="post">
+                    <h2>Publications</h2>
+
+                    <div>
+                        <h3>
+                            2016
+                        </h3>
+                        <ul>
+
+                                    <li>
+                                    <p>
+                                    <strong>Pooyan Behnamghader</strong>, Duc Le, Joshua Garcia, Daniel Link, Arman Shahbazian and Nenad Medvidovic.
+                                    “A Large-Scale Study of Architectural Evolution in Open-Source Software Systems.” Journal of Empirical Software Engineering. In Press.
+                                    <!--
+                                        M. Moghaddami, <strong>P. Behnamghader</strong>, M. Raisee, "Effects of vortex shedding on the UV-H2O2 photoreactor performance"    <strong>to be submitted</strong>
+                                    -->
+                                     <!-- Buffer -->
+                                    
+                                    <!--
+                                    <div class="workDescription">
+                                    <ul >
+                                        <li>
+                                            <b>Abstract</b>—Presentation failures in a website can undermine its success by giving users a negative perception of the trustwor- thiness of the site and the quality of the services it delivers. Unfortunately, existing techniques for debugging presentation failures do not provide developers with automated and broadly applicable solutions for finding the site’s faulty HTML elements and CSS properties. To address this limitation, we propose a novel automated approach for debugging web sites that is based on image processing and probabilistic techniques. Our approach first builds a model that links observable changes in the web site’s appearance to faulty elements and styling properties. Then using this model, our approach predicts the elements and styling properties most likely to cause the observed failure for the page under test and reports these to the developer. In evaluation, our approach was more accurate and faster than prior techniques for identifying faulty elements in a website.
+                                        
+                                        </li>
+                                        
+                                    </ul>
+                                     </div>
+                                        -->
+                                    </p>
+                                </li>
+
+                                <li>
+                                    <p>
+
+                                    Sonal Mahajan, Bailan Li, <strong>Pooyan Behnamghader</strong>, William G. J. Halfond. “Using Visual Symptoms for Debugging Presentation Failures in Web Applications.” In <i>Proceedings of the 9th IEEE International Conference on Software Testing, Verification and Validation (ICST 2016),</i> Chicago, USA, April 2016.
+                                    <!--
+                                        M. Moghaddami, <strong>P. Behnamghader</strong>, M. Raisee, "Effects of vortex shedding on the UV-H2O2 photoreactor performance"    <strong>to be submitted</strong>
+                                    -->
+                                     <!-- Buffer -->
+                                    
+                                    <!--
+                                    <div class="workDescription">
+                                    <ul >
+                                        <li>
+                                            <b>Abstract</b>—Presentation failures in a website can undermine its success by giving users a negative perception of the trustwor- thiness of the site and the quality of the services it delivers. Unfortunately, existing techniques for debugging presentation failures do not provide developers with automated and broadly applicable solutions for finding the site’s faulty HTML elements and CSS properties. To address this limitation, we propose a novel automated approach for debugging web sites that is based on image processing and probabilistic techniques. Our approach first builds a model that links observable changes in the web site’s appearance to faulty elements and styling properties. Then using this model, our approach predicts the elements and styling properties most likely to cause the observed failure for the page under test and reports these to the developer. In evaluation, our approach was more accurate and faster than prior techniques for identifying faulty elements in a website.
+                                        
+                                        </li>
+                                        
+                                    </ul>
+                                </div>
+                                    -->
+                                    </p>
+                                </li>
+
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>
+                            2015
+                        </h3>
+                        
+                            <ul>
+                                <li>
+                                    <p>
+
+                                    Duc Le*, <strong>Pooyan Behnamghader* (co-first author)</strong>, Joshua Garcia, Daniel Link, Arman Shahbazian and Nenad Medvidovic. “<a href="publications/emparch_msr15.pdf">An Empirical Study of Architectural Change in Open-Source Software Systems</a>.” In <i>Proceedings of the 12th Working Conference on Mining Software Repositories (MSR 2015),</i> Florence, Italy, May 2015.
+                                    <!--
+                                        M. Moghaddami, <strong>P. Behnamghader</strong>, M. Raisee, "Effects of vortex shedding on the UV-H2O2 photoreactor performance"    <strong>to be submitted</strong>
+                                    -->
+                                     <!-- Buffer -->
+                                     (<a href="publications/emparch_msr15.pdf"><b>Download</b></a>)
+                                    <!--
+                                    <div class="workDescription">
+                                    <ul >
+                                        <li>
+                                        <b>Abstract</b>-From its very inception, the study of software architecture has recognized architectural decay as a regularly occurring phenomenon in long-lived systems. Architectural decay
+is caused by repeated changes to a system during its lifespan. Despite decay’s prevalence, there is a relative dearth of empirical data regarding the nature of architectural changes that may lead
+to decay, and of developers’ understanding of those changes. In this paper, we take a step toward addressing that scarcity by conducting an empirical study of changes found in software
+architectures spanning several hundred versions of 14 opensource systems. Our study reveals several new findings regarding the frequency of architectural changes in software systems, the
+common points of departure in a system’s architecture during maintenance and evolution, the difference between system-level and component-level architectural change, and the suitability
+of a system’s implementation-level structure as a proxy for its architecture.
+                                        </li>
+                                        
+                                    </ul>
+                                     </div>
+-->
+                                    </p>
+                                </li>
+
+                                
+                            </ul>
+                    
+                    </div>
+                    <p class="justified">
+                    (Updated January 2016)
+                    </p>
+                </div>
+            </div>
+        </div>
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
--- topmenu.php
+++ topmenu.php
@@ -7,7 +7,7 @@
         -->
         <a href="PooyanBehnamghader.pdf">CV</a><span class="hide"> | </span>
         <a href="publications.php">Publications</a><span class="hide"> | </span>
-        <a href="publications.php">Research Projects</a><span class="hide"> | </span>
+        <a href="research_projects.php">Research Projects</a><span class="hide"> | </span>
         <!--
         <a href="courses.php">Related Courses</a><span class="hide"> | </span>
         <a href="photos.php">Photos</a><span class="hide"> | </span>
