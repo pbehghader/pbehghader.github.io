22a70fbe5b01d0658d44d02830daab3009c11466
pooyan <behnamghader@gmail.com>
2016-07-04 18:32:08 -0700

    update the research projects

--- research_projects.php
+++ research_projects.php
@@ -26,7 +26,7 @@
                         </h3>
                             ARCADE is a software workbench that employs a suite of architecture recoverytechniques and metrics for different aspects of architectural change.
                             <br/>
-                            My contributions in ARCADE projects are as follows:
+                            My contributions in ARCADE project are as follows:
                             <ul>
                                 <li>
                                     Improving ACDC architecture recovery technique.
@@ -42,45 +42,19 @@
 
                     <div>
                         <h3>
-                            2015
+                            SoftEvo
                         </h3>
                             
-Developing a toolset to use cloud computing in order to semi-automatically run large-scale software
-architecture recovery analyses on the history of systems in software repositories
-1
-Improving Mahjong, a distributed software system that uses idle cycles on remote but networked
-computers to solve NP-complete problems
-
+                            SoftEvo is a toolset to use cloud computing in order to semi-automatically run large-scale static and dynamic programming analyses on the history of systems in software repositories.
+                            <br/>
+                            My contributions in SoftEvo project is as follows:
                             <ul>
                                 <li>
-                                    <p>
-
-                                    Duc Le*, <strong>Pooyan Behnamghader* (co-first author)</strong>, Joshua Garcia, Daniel Link, Arman Shahbazian and Nenad Medvidovic. “<a href="publications/emparch_msr15.pdf">An Empirical Study of Architectural Change in Open-Source Software Systems</a>.” In <i>Proceedings of the 12th Working Conference on Mining Software Repositories (MSR 2015),</i> Florence, Italy, May 2015.
-                                    <!--
-                                        M. Moghaddami, <strong>P. Behnamghader</strong>, M. Raisee, "Effects of vortex shedding on the UV-H2O2 photoreactor performance"    <strong>to be submitted</strong>
-                                    -->
-                                     <!-- Buffer -->
-                                     (<a href="publications/emparch_msr15.pdf"><b>Download</b></a>)
-                                    <!--
-                                    <div class="workDescription">
-                                    <ul >
-                                        <li>
-                                        <b>Abstract</b>-From its very inception, the study of software architecture has recognized architectural decay as a regularly occurring phenomenon in long-lived systems. Architectural decay
-is caused by repeated changes to a system during its lifespan. Despite decay’s prevalence, there is a relative dearth of empirical data regarding the nature of architectural changes that may lead
-to decay, and of developers’ understanding of those changes. In this paper, we take a step toward addressing that scarcity by conducting an empirical study of changes found in software
-architectures spanning several hundred versions of 14 opensource systems. Our study reveals several new findings regarding the frequency of architectural changes in software systems, the
-common points of departure in a system’s architecture during maintenance and evolution, the difference between system-level and component-level architectural change, and the suitability
-of a system’s implementation-level structure as a proxy for its architecture.
-                                        </li>
-                                        
-                                    </ul>
-                                     </div>
--->
-                                    </p>
+                                    I designed and implemented SoftEvo.
                                 </li>
-
-                                
                             </ul>
+                            Improving Mahjong, a distributed software system that uses idle cycles on remote but networked
+computers to solve NP-complete problems
                     
                     </div>
                     <p class="justified">
