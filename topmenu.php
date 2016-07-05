f09a439e36a58d1feaf7ac6ccd3465b16646bf64
pooyan <behnamghader@gmail.com>
2016-07-04 18:15:03 -0700

    update the top menu

--- topmenu.php
+++ topmenu.php
@@ -5,6 +5,7 @@
         <a href="about.php">About</a><span class="hide"> | </span>
         <a href="PooyanBehnamghader.pdf">CV</a><span class="hide"> | </span>
         <a href="publications.php">Publications</a><span class="hide"> | </span>
+        <a href="publications.php">Research Projects</a><span class="hide"> | </span>
         <!--
         <a href="courses.php">Related Courses</a><span class="hide"> | </span>
         <a href="photos.php">Photos</a><span class="hide"> | </span>
