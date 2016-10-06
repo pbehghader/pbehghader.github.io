ae697a8a71c712c604aad27c96e6a5b77f833037
pooyan <behnamghader@gmail.com>
2016-10-06 13:04:43 -0700

    push

--- Pooyan/header.php
+++
@@ -1,9 +0,0 @@
-<div id="header">
-    <div id="title">
-        <h1>Pooyan Behnamghader  پویان بهنام قادر</h1>
-        <i>
-            <p class="headerFirst"><a target="_blank" style="border-bottom: none;" href="http://softarch.usc.edu/">Software Architecture Research Group</a>, <a target="_blank" style="border-bottom: none;" href="http://sunset.usc.edu/">Center for Systems and Software Engineering</a></p>
-            <p class="headerSecond"><a target="_blank" style="border-bottom: none" href="http://www.cs.usc.edu/">Department of Computer Science</a>, <a target="_blank" style="border-bottom: none" href="http://viterbi.usc.edu/">Viterbi School of Engineering</a>, <a target="_blank" style="border-bottom: none" href="http://www.usc.edu/">University of Southern California</a></p>
-        </i>
-    </div>
-</div>
--- Pooyan/persianstat.php
+++
@@ -1,7 +0,0 @@
-<!-- PersianStat -->
-<!-- URL: http://behnamghader.net -->
-<script language='javascript' type='text/javascript' src='http://www.persianstat.com/service/stat.js'></script>
-<script language='javascript' type='text/javascript'>
-persianstat(10174625, 0);
-</script>
-<!-- /PersianStat -->
