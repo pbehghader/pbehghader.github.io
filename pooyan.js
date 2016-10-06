abaf34e38858773875dec780fab39de439211899
pooyan <behnamghader@gmail.com>
2016-10-06 11:45:09 -0700

    change in home

--- pooyan.js
+++ pooyan.js
@@ -1,5 +1,5 @@
-function(){ $("#includedHeader").load("header.html"); }
-function(){ $("#includedTopMenu").load("topmenu.html"); }
-function(){ $("#includedSideBar").load("sidebar.html"); }
-function(){ $("#includedFooter").load("footer.html"); }
-function(){ $("#includedAnalytics").load("analytics.html"); }
+$(function(){ $("#includedHeader").load("header.html"); });
+$(function(){ $("#includedTopMenu").load("topmenu.html"); });
+$(function(){ $("#includedSideBar").load("sidebar.html"); });
+$(function(){ $("#includedFooter").load("footer.html"); });
+$(function(){ $("#includedAnalytics").load("analytics.html"); });
