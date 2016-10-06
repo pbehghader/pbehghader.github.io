4c709f4997271435a677de2fea40944b59817534
pooyan <behnamghader@gmail.com>
2016-10-06 11:33:35 -0700

    change in home

---
+++ pooyan.js
@@ -0,0 +1,5 @@
+<script> $(function(){ $("#includedHeader").load("header.html"); }); </script> 
+<script> $(function(){ $("#includedTopMenu").load("topmenu.html"); }); </script> 
+<script> $(function(){ $("#includedSideBar").load("sidebar.html"); }); </script> 
+<script> $(function(){ $("#includedFooter").load("footer.html"); }); </script> 
+<script> $(function(){ $("#includedAnalytics").load("analytics.html"); }); </script> 
